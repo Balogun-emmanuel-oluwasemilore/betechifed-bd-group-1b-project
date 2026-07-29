166b3629ac53e07e2d9ea20f9622458cd68c1fc9
Daniel <adebamipedaniel@gmail.com>
2026-07-29 16:24:24 +0200

    Add DELETE/tasks endpoint

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -9,4 +9,27 @@ router.post('/', validateTask, taskController.createTask);
 router.put('/:id', validateTask, taskController.updateTask);
 router.delete('/:id', taskController.deleteTask);
 
+
+router.delete("/tasks/:id", (req, res) => {
+
+    const id = parseInt(req.params.id);
+
+    const index = tasks.findIndex(task => task.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({
+            success: false,
+            message: "Task not found"
+        });
+    }
+
+    const deletedTask = tasks.splice(index, 1);
+
+    res.status(200).json({
+        success: true,
+        data: deletedTask[0]
+    });
+
+});
+
 module.exports = router;
